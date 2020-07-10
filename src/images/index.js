37863be5e8ffa9f415e6943a3f0af32f285696fc
Joshua Drubin <j.drubin94@gmail.com>
2020-07-10 10:43:23 -0700

    update tile images

--- src/images/index.js
+++ src/images/index.js
@@ -1,25 +1,3 @@
-// import block0 from './block_0.png';
-// import block1 from './block_1.png';
-// import block2 from './block_2.png';
-// import block3 from './block_3.png';
-// import grassBlock0 from './grass_block_0.png';
-// import grassBlock1 from './grass_block_1.png';
-// import grassBlock2 from './grass_block_2.png';
-// import snowBlock0 from './snow_block_0.png';
-// import snowBlock1 from './snow_block_1.png';
-
-// export {
-//     block0,
-//     block1,
-//     block2,
-//     block3,
-//     grassBlock0,
-//     grassBlock1,
-//     grassBlock2,
-//     snowBlock0,
-//     snowBlock1,
-// };
-
 import grassTile from './iso_grass_tile_cropped.png';
 import waterTile from './iso_water_tile_cropped.png';
 
