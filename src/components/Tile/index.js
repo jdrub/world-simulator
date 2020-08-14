bd50e9fa17683b8c491457efa9906ccd807a2c5d
Joshua Drubin <j.drubin94@gmail.com>
2020-08-14 02:48:05 -0400

    add clipping planes. they wont work as a final soln

--- src/components/Tile/index.js
+++ src/components/Tile/index.js
@@ -2,45 +2,63 @@ import React, { useRef, useState } from 'react';
 import { Canvas, useLoader, useFrame, useThree } from "react-three-fiber"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 import { useEffect, useMemo } from 'react';
+import * as THREE from 'three';
 
 export default ({ pos }) => {
     const mesh = useRef();
 
-    // const position = [0,0,0];
-
     const gltf = useLoader(GLTFLoader, "/grass_tile.glb")
     
+    // useFrame(() => {
+    //     mesh.current.rotation.x += 0.02;
+    //     mesh.current.rotation.y -= 0.01;
+    //     mesh.current.rotation.z += 0.02;
+    //     mesh.current.position.x += 0.05;
+    // });
 
-    // raf loop
-    useFrame(() => {
-        // mesh.current.rotation.x += 0.02;
-        // mesh.current.rotation.y -= 0.01;
-        // mesh.current.rotation.z += 0.02;
-        // mesh.current.position.x += 0.01;
-    });
+    const { scene, gl } = useThree();
+
+    const clippingPlanes = useMemo(() => [
+        new THREE.Plane(new THREE.Vector3(0, 0, 1), 5),
+        new THREE.Plane(new THREE.Vector3(0, 0, -1), 5),
+        new THREE.Plane(new THREE.Vector3(1, 0, 0), 5),
+        new THREE.Plane(new THREE.Vector3(-1, 0, 0), 5),
+    ], []);
+
+    useEffect(() => {
+        // const helpers = new THREE.Group();
+        // helpers.add( new THREE.PlaneHelper( clippingPlanes[ 0 ], 2, 0xff0000 ) );
+        // helpers.add( new THREE.PlaneHelper( clippingPlanes[ 1 ], 2, 0xff0000 ) );
+        // helpers.add( new THREE.PlaneHelper( clippingPlanes[ 2 ], 2, 0x0000ff ) );
+        // helpers.add( new THREE.PlaneHelper( clippingPlanes[ 3 ], 2, 0x0000ff ) );
+        // helpers.visible = true;
+        // scene.add( helpers );
+        gl.localClippingEnabled = true;
+    }, []);
 
     return (
         <group
             ref={mesh}
             position={[pos.row*2.1, 0, pos.col * 2.1]}
         >
-            <mesh {...gltf.nodes.Cube} />
-            <mesh {...gltf.nodes.Cube001} />
-            <mesh {...gltf.nodes.Cube002} />
+            <mesh {...{ ...gltf.nodes.Cube, material: undefined}}>
+                <meshPhysicalMaterial
+                    attach="material"
+                    {...gltf.nodes.Cube.material}
+                    clippingPlanes={clippingPlanes} />
+            </mesh>
+            <mesh {...gltf.nodes.Cube001}>
+                <meshPhysicalMaterial
+                    attach="material"
+                    {...gltf.nodes.Cube001.material}
+                    clippingPlanes={clippingPlanes} />
+            </mesh>
+            <mesh {...gltf.nodes.Cube002}>
+                <meshPhysicalMaterial
+                    attach="material"
+                    {...gltf.nodes.Cube002.material}
+                    clippingPlanes={clippingPlanes} />
+            </mesh>
         </group>
-        
-        // <mesh
-        //   ref={mesh}
-        //   position={[pos.row, 0, pos.col]}
-        //   visible
-        // >
-        //     <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-        //     <meshStandardMaterial
-        //         attach="material"
-        //         color="white"
-        //         roughness={0.3}
-        //         metalness={0.3} />
-        // </mesh>
-        
       );
 }
--- src/components/WorldMap/WorldMapView.js
+++ src/components/WorldMap/WorldMapView.js
@@ -1,18 +1,13 @@
 import React, { useMemo, Suspense, useRef } from 'react';
-import styled, { css } from 'styled-components';
 import Tile from '../Tile';
 import Lights from '../Lights';
-
-import { Canvas, useFrame } from "react-three-fiber";
-import { useEffect } from 'react';
-import MapBounds from '../MapBounds';
 
 const WorldMapView = ({ position, fullTileMap, updateTile }) => {
 
     const visibleTiles = useMemo(() => {
         let newTilemap = [];
-        for(let row = -2; row <= 2; row++) {
-            for (let col = -2; col <= 2; col++) {
+        for(let row = -15; row <= 15; row++) {
+            for (let col = -15; col <= 15; col++) {
                 newTilemap.push(<Tile key={`${row},${col}`} pos={{row, col}} />);
             }
         }
@@ -20,10 +15,6 @@ const WorldMapView = ({ position, fullTileMap, updateTile }) => {
     }, [position.row, position.col]);
 
     const tilesRef = useRef();
-
-    useFrame(() => {
-        // tilesRef.current.rotation.y -= 0.01;
-    })
 
     return (
         <>
@@ -33,7 +24,6 @@ const WorldMapView = ({ position, fullTileMap, updateTile }) => {
                 </Suspense>
             </group>
             <Lights />
-            {/* <MapBounds /> */}
         </>
     );
 }
